9060d421f4dffe3d003f10cc367b88fe54c070d7
UrNot42 <ulevallo@student.42.fr>
2024-01-25 16:48:02 +0100

    [EDIT] push version

---
+++ CPP-00/ex00/megaphone.cpp
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ulevallo <ulevallo@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/29 10:45:36 by ulevallo          #+#    #+#             */
+/*   Updated: 2023/11/30 19:52:21 by ulevallo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+
+int	main(int ac, char **av)
+{
+	if (ac < 2)
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+	else
+	{
+		std::string	upper_str;
+		for (int i = 1; av[i]; i++)
+		{
+			upper_str += av[i];
+		}
+		for (int i = 0; upper_str[i] ; i++)
+		{
+			upper_str[i] = std::toupper(upper_str[i]);
+		}
+		std::cout << upper_str << std::endl;
+	}
+}
